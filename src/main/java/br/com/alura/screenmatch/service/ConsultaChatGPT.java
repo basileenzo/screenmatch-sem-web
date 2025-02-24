d63d1649990c5f7eea8af8a88e0add8a9bf5be44
basileenzo <basileenzo22@gmail.com>
2025-02-24 10:41:50 -0300

    Variaveis de ambiente, insert serie, read serie

--- src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
+++ src/main/java/br/com/alura/screenmatch/service/ConsultaChatGPT.java
@@ -5,7 +5,7 @@ import com.theokanning.openai.service.OpenAiService;
 
 public class ConsultaChatGPT {
     public static String obterTraducao(String texto) {
-        OpenAiService service = new OpenAiService("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");
+        OpenAiService service = new OpenAiService(System.getenv("OPENAI_APIKEY"));
 
         CompletionRequest requisicao = CompletionRequest.builder()
                 .model("gpt-3.5-turbo-instruct")
